b1ebd550c671c8cf7ea4d6ef1f8506f25ab77ac8
TotallyInformatik <totallyturing@gmail.com>
2025-04-13 12:28:56 +0200

    dashboard APi is now connected (should be)

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -4,14 +4,22 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { SelectTrigger, Select, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
-import { useRef } from "react"
+import { FormEvent, useEffect, useRef, useState } from "react"
 import { addDoc, collection, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore"
 import { appFirestore } from "@/lib/firebase/db"
+import { keyframes } from "motion/react"
 
 export default function Page() {
   const videoRef = useRef<HTMLVideoElement>(null!);
+  const inputRef = useRef<HTMLInputElement>(null!);
+  const [selectedAgentIndex, setSelectedAgentIndex] = useState<number>(-1);
+  const [agents, setAgents] = useState<{
+    robotId: string,
+    task_name: string
+  }[]>([]);
 
   const valueChanged = (e: string) => {
+    setSelectedAgentIndex(parseInt(e));
     (async () => {
           const remoteStream = new MediaStream();
           const pc = new RTCPeerConnection();
@@ -63,6 +71,36 @@ export default function Page() {
     })();
   }
 
+  useEffect(() => {
+    (async () => {
+      const robots = await fetch("http://168.119.174.209:8000/v1/robots");
+      const robotList = await robots.json();
+      console.log(robotList);
+      setAgents(robotList);
+    })()
+  }, [])
+
+  const submitPrompt = (e: FormEvent<HTMLFormElement>) => {
+ 
+    e.preventDefault();
+    if (selectedAgentIndex == -1) return;
+    const prompt = inputRef.current.value;
+    inputRef.current.value = "";
+    const robotId = agents[selectedAgentIndex].robotId;
+    fetch(`http://168.119.174.209:8000/v1/robots/${robotId}/task`, {
+      method: "POST",
+      body: JSON.stringify({
+        robot_id: robotId,
+        task: {
+          name: prompt,
+          system_prompt: prompt
+        }
+      })
+    })
+
+  }
+
+
   return <section className="w-full h-full min-h-screen bg-black text-white p-5 flex flex-col justify-between">
     <h1 className="text-3xl">Good Afternoon, Mr. Hwang</h1>
     <section className="w-full my-5 h-[800px] overflow-hidden"  style={{
@@ -71,18 +109,21 @@ export default function Page() {
     }}>
       <video ref={videoRef} autoPlay playsInline className="w-full"/>
     </section>
-    <form className="w-full grid grid-cols-10 grid-rows-2 gap-x-5 gap-y-3 items-end">
+    <form className="w-full grid grid-cols-10 grid-rows-2 gap-x-5 gap-y-3 items-end" onSubmit={submitPrompt}>
       <Label htmlFor="prompt" className="w-full row-start-1 row-end-1 col-start-1 col-end-7 self-end h-fit">Prompt</Label>
       <Label htmlFor="agent" className="w-full row-span-1 col-start-7 col-end-10 self-end h-fit">Agent</Label>
-      <Input name="prompt" placeholder="Enter your Prompt here" className="row-start-2 row-end-2 col-start-1 col-end-7 "/>
+      <Input ref={inputRef} name="prompt" placeholder="Enter your Prompt here" className="row-start-2 row-end-2 col-start-1 col-end-7 "/>
       <div className="row-start-2 row-end-2 col-start-7 col-end-10 ">
         <Select name="agent" onValueChange={(e) => valueChanged(e)}>
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Agent" />
           </SelectTrigger>
           <SelectContent className="bg-black text-white">
-            <SelectItem value="1">Agent 1</SelectItem>
-            <SelectItem value="2">Agent 2</SelectItem>
+            {
+              agents.map((agent, idx) => {
+                return <SelectItem key={idx} value={idx.toString()}>Agent {agent.robotId}</SelectItem>
+              })
+            }
           </SelectContent>
         </Select>
       </div>
